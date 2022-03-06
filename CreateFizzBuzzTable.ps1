f60fca825380ed06acdb372b5fce3027f4fbff3c
Greg Zhou <dakaizhou.gz@gmail.com>
2022-03-06 13:39:26 +1300

    Chage inline SQL query to external sql file

--- CreateFizzBuzzTable.ps1
+++ CreateFizzBuzzTable.ps1
@@ -4,26 +4,8 @@
 #Install-Module SqlServer
 Import-Module SqlServer
 
-$SQLquery = "
-CREATE TABLE [dbo].[FizzBuzzResults] (
-    N NVARCHAR(8)
-);
-
-DECLARE @N INT
-SET @N = 1
-
-WHILE @N <= 100
-BEGIN
-INSERT INTO [dbo].[FizzBuzzResults] (N)
-SELECT @N
-SET @N = @N + 1
-END
-
-UPDATE [dbo].[FizzBuzzResults] SET N = 'FizzBuzz' WHERE ((N % 3 = 0) and (N % 5 = 0))
-UPDATE [dbo].[FizzBuzzResults] SET N = 'Fizz' WHERE (N % 3 = 0) and (N != 'FizzBuzz') and (N != 'Buzz')
-UPDATE [dbo].[FizzBuzzResults] SET N = 'Buzz' WHERE (N % 5 = 0) and (N != 'FizzBuzz') and (N != 'Fizz')
-"
+$sqlFile = "SQLQuery_1.sql"
 $pw = "Something1"
 $dbConnectionString = "Server=tcp:database1-server.database.windows.net,1433;Initial Catalog=database1-db;Persist Security Info=False;User ID=test;Password=$pw;MultipleActiveResultSets=False;Encrypt=True;TrustServerCertificate=False;Connection Timeout=30;"
 
-Invoke-Sqlcmd -Query $SQLquery -ConnectionString $dbConnectionString -Verbose
+Invoke-Sqlcmd -InputFile $sqlFile -ConnectionString $dbConnectionString -Verbose
